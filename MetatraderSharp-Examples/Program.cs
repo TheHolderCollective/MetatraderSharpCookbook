613d1df234da8246a13c35743e87e3278696917a
R Holder <101442317+TheHolderCollective@users.noreply.github.com>
2026-05-19 00:46:15 -0700

    Add project files.

---
+++ MetatraderSharp-Examples/Program.cs
@@ -0,0 +1,25 @@
+﻿using MetatraderSharp;
+using MetatraderSharp.MTsocketAPI.Responses;
+namespace MetatraderSharp_Examples;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        MetatraderClient mtClient = new();
+
+        try
+        {
+            Account? myAccount = mtClient.GetAccountInfo();
+
+            Console.WriteLine(myAccount);
+        }
+        catch (Exception ex)
+        {
+            string exceptionName = ex.GetType().ToString();
+
+            Console.WriteLine($"{exceptionName}: {ex.Message}");
+        }
+    }
+}
---
+++ Recipe-GetTerminal_Info/Program.cs
@@ -0,0 +1,24 @@
+﻿using MetatraderSharp;
+using MetatraderSharp.MTsocketAPI.Responses;
+namespace Recipe_GetTerminal_Info;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        MetatraderClient mtClient = new();
+
+        try
+        {
+            TerminalInfo? myTerminal = mtClient.GetTerminalInfo();
+
+            Console.WriteLine(myTerminal);
+        }
+        catch (Exception ex)
+        {
+            string exceptionName = ex.GetType().ToString();
+
+            Console.WriteLine($"{exceptionName}: {ex.Message}");
+        }
+    }
+}
