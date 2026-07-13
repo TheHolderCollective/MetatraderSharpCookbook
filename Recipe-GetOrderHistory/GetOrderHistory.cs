1f8dd7c613b3e6471a9831f6c304281ac30f2dfe
R Holder <101442317+TheHolderCollective@users.noreply.github.com>
2026-07-13 13:12:04 -0700

    Action: Added Get Order History recipe.

---
+++ Recipe-GetOrderHistory/GetOrderHistory.cs
@@ -0,0 +1,48 @@
+﻿using MetatraderSharp;
+using MetatraderSharp.MetatraderClient;
+using MetatraderSharp.MTsocketAPI.Responses;
+using MetatraderSharp.MTsocketAPI.Responses.MT4;
+namespace Recipe_GetOrderHistory;
+
+/// <summary>
+/// Recipe showing how to get order history
+/// </summary>
+public class GetOrderHistory
+{
+    static async Task Main(string[] args)
+    {
+        MT4Client mtClient = new();
+
+        try
+        {
+            if (!mtClient.StatusIsOK)
+            {
+                Console.WriteLine("Unable to connect to request URI.");
+                return;
+            }
+
+            DateTime currentDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 23, 59, 59);
+            DateTime previousMonth = new DateTime(currentDate.Year, currentDate.Month - 1, currentDate.Day);
+
+            string toDate = currentDate.ToString();
+            string fromDate = previousMonth.ToString();
+
+            OrderHistory orderHistory = await mtClient.GetOrderHistoryAsync(fromDate, toDate);
+
+            if (mtClient.LastQueryStatus == QueryStatus.OK)
+            {
+                Console.WriteLine($"Order History from [{fromDate}] to [{toDate}]: ");
+                Console.WriteLine(orderHistory);
+            }
+
+            Console.WriteLine($"\nQueryStatus = {mtClient.LastQueryStatus}");
+            Console.WriteLine($"QueryMessage = {mtClient.LastQueryMessage}\n");
+        }
+        catch (Exception ex)
+        {
+            string exceptionName = ex.GetType().ToString();
+            Console.WriteLine($"{exceptionName}: {ex.Message}");
+        }
+
+    }
+}
