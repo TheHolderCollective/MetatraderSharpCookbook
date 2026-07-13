1d91f774235377c28d557d715617e48d4c7832bf
R Holder <101442317+TheHolderCollective@users.noreply.github.com>
2026-07-13 15:35:07 -0700

    Action: Added Get Order Info recipe

---
+++ Recipe-GetOrderInfo/GetOrderInfo.cs
@@ -0,0 +1,50 @@
+﻿using MetatraderSharp;
+using MetatraderSharp.MetatraderClient;
+using MetatraderSharp.MTsocketAPI.Responses.MT4;
+namespace Recipe_GetOrderInfo;
+
+/// <summary>
+/// Recipe showing how to get order information
+/// </summary>
+class GetOrderInfo
+{
+    static async Task Main(string[] args)
+    {
+        MT4Client mtClient = new();
+        long ticketNumber;
+
+        try
+        {
+            if (!mtClient.StatusIsOK)
+            {
+                Console.WriteLine("Unable to connect to request URI.");
+                return;
+            }
+
+            Console.Write("Please enter a valid ticket number: ");
+            string? userResponse = Console.ReadLine();
+            ticketNumber = Convert.ToInt64(userResponse);
+
+            OrderInfo orderInfo = await mtClient.GetOrderInfoAsync(ticketNumber);
+
+            if (mtClient.LastQueryStatus == QueryStatus.OK)
+            {
+                Console.WriteLine($"\nOrder Info - Ticket {ticketNumber}: ");
+            }
+
+            Console.WriteLine(orderInfo);
+
+            Console.WriteLine($"\nQueryStatus = {mtClient.LastQueryStatus}");
+            Console.WriteLine($"QueryMessage = {mtClient.LastQueryMessage}\n");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Input is not a valid ticket number.");
+        }
+        catch (Exception ex)
+        {
+            string exceptionName = ex.GetType().ToString();
+            Console.WriteLine($"{exceptionName}: {ex.Message}");
+        }
+    }
+}
