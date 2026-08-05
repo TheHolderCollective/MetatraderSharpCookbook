9f834cf96dbb0e1773dd964d2af4766e89571477
R Holder <101442317+TheHolderCollective@users.noreply.github.com>
2026-08-05 14:26:04 -0700

    Action: Added Get Calendar recipe.

---
+++ MT5/Recipe-GetCalendar/GetCalendar.cs
@@ -0,0 +1,41 @@
+﻿using MetatraderSharp.MTsocketAPI.Responses.MT5;
+using MetatraderSharp.MetatraderClient;
+namespace Recipe_GetCalendar;
+
+/// <summary>
+/// Recipe showing how to get calendar info
+/// This is only available in Metatrader 5
+/// </summary>
+public class GetCalendar
+{
+    static async Task Main(string[] args)
+    {
+        MT5Client mtClient = new();
+
+        try
+        {
+            if (!mtClient.StatusIsOK)
+            {
+                Console.WriteLine("Unable to connect to request URI.");
+                return;
+            }
+
+            string fromDate = "2025.01.26 21:15:00";
+            string toDate = "2025.01.30 21:15:00";
+            string countryCode = "US";
+            string currency = "USD"; // this is optional
+
+            Calendar usCalendar = await mtClient.GetCalendarAsync(fromDate, toDate, countryCode, currency);
+
+            Console.WriteLine($"Calendar events for {countryCode} from {fromDate} to {toDate}:");
+            Console.WriteLine(usCalendar);
+
+        }
+        catch (Exception ex)
+        {
+            string exceptionName = ex.GetType().ToString();
+            Console.WriteLine($"{exceptionName}: {ex.Message}");
+        }
+    }
+}
+
