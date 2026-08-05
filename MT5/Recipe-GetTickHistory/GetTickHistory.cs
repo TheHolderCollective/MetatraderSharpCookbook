e33175b64ce62038150e9a9d3ce8763fe9b1cfe5
R Holder <101442317+TheHolderCollective@users.noreply.github.com>
2026-08-05 15:16:12 -0700

    Action: Added Get Tick History recipe.

---
+++ MT5/Recipe-GetTickHistory/GetTickHistory.cs
@@ -0,0 +1,37 @@
+﻿using MetatraderSharp.MetatraderClient;
+using MetatraderSharp.MTsocketAPI.Responses;
+using MetatraderSharp.MTsocketAPI.Responses.MT5;
+namespace Recipe_GetTickHistory;
+
+public class GetTickHistory
+{
+    static async Task Main(string[] args)
+    {
+        MT5Client mtClient = new();
+
+        try
+		{
+            if (!mtClient.StatusIsOK)
+            {
+                Console.WriteLine("Unable to connect to request URI.");
+                return;
+            }
+
+            string fromDate = "2025.09.30 20:05:00";
+            string toDate = "2025.09.30 20:10:00";
+            string symbol = "EURUSD";
+
+            TickHistory tickHistory = await mtClient.GetTickHistoryAsync(fromDate, toDate, symbol, TickFlag.COPY_TICKS_ALL);
+
+            Console.WriteLine($"Tick history for {symbol} from {fromDate} to {toDate}:");
+            Console.WriteLine(tickHistory);
+
+        }
+		catch (Exception ex)
+        {
+            string exceptionName = ex.GetType().ToString();
+            Console.WriteLine($"{exceptionName}: {ex.Message}");
+        }
+    }
+}
+
