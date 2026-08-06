fca4fef26f2ad961bec34495b0b8f8e24ed61995
R Holder <101442317+TheHolderCollective@users.noreply.github.com>
2026-08-06 13:26:13 -0700

    Action: Added Get Order List recipe.

---
+++ MT5/Recipe-GetOrderList/GetOrderList.cs
@@ -0,0 +1,34 @@
+﻿using MetatraderSharp.MetatraderClient;
+using MetatraderSharp.MTsocketAPI.Responses.MT5;
+namespace Recipe_GetOrderList;
+
+public class GetOrderList
+{
+    static async Task Main(string[] args)
+    {
+        MT5Client mtClient = new();
+
+        try
+		{
+
+            if (!mtClient.StatusIsOK)
+            {
+                Console.WriteLine("Unable to connect to request URI.");
+                return;
+            }
+
+            OrderList orderList = await mtClient.GetOrderListAsync();
+
+            Console.WriteLine("Current order list: ");
+            Console.WriteLine(orderList);
+
+        }
+		catch (Exception ex)
+		{
+            string exceptionName = ex.GetType().ToString();
+            Console.WriteLine($"{exceptionName}: {ex.Message}");
+        }
+       
+    }
+}
+
